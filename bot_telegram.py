ab9e20cbd0ce39fa1d31aaa5051956d2dd597dcf
AntonUmarov <arumarov@yandex.ru>
2022-03-27 14:06:07 +0300

    end lesson 4

--- bot_telegram.py
+++ bot_telegram.py
@@ -4,5 +4,11 @@ from create_bot import dp
 async def on_startup(_): # пишем функцию, которая запускается при выходе бота в онлайн(без декоратора) 
     print('Бот вышел в онлайн') # далее, ниже передаем эту функцию в executor, функция выводит сообщение в консоль
 
+from handlers import client, admin, other # импортируем файлы
+
+client.register_handlers_client(dp)
+other.register_handlers_other(dp)
+
+
 executor.start_polling(dp, skip_updates = True, on_startup = on_startup) # записываем команду запуска бота
 # skip_updates = True - команда, чтобы бот не отвечал на сообщения, которые приходят, пока бот офлайн
--- handlers/client.py
+++ handlers/client.py
@@ -1,20 +1,20 @@
-from aiogram import types # импортируем типы данных
+from aiogram import types, Dispatcher # импортируем типы данных
 from create_bot import dp, bot
 
-@dp.message_handler(commands = ['start', 'help']) # указываем декоратор и в аргументах указываем команды, на которые будет реагировать нащ бот
+# @dp.message_handler(commands = ['start', 'help']) # указываем декоратор и в аргументах указываем команды, на которые будет реагировать нащ бот
 # сработает, когда пользователь напишет команду /start либо команду /help. Handler сработает как только пользователь добавится к нашему боту
-async def echo_send(message : types.Message):
+async def command_start(message : types.Message):
     try: # обрабатываеи ошибку (если не получится отправить сообщение пользователю - если он не добавил бота)
         await bot.send_message(message.from_user.id, 'Приятного аппетита') # записываем код, который сработает после команды /start либо команды /help.
         await message.delete() # будем удалять сообщение, если оно не доставлено
     except: # не будем указывать конкретную ошибку
         await message.reply('Общение с ботом через ЛС, напишите ему:\nhttps://t.me/Pizza_arumarov_bot') # данное условие выполнится, если пользователь еще не добавлялся к боту
 # добавляем далее все необходимые нам обработчики команд
-@dp.message_handler(commands = ['Режим_работы'])
+# @dp.message_handler(commands = ['Режим_работы'])
 async def pizza_open_command(message: types.Message):
     await bot.send_message(message.from_user.id, 'Вс-Чт с 9:00 до 20:00, Пт-Сб с 10:00 до 23:00')
 
-@dp.message_handler(commands = ['Расположение'])
+# @dp.message_handler(commands = ['Расположение'])
 async def pizza_place_command(message : types.Message):
     await bot.send_message(message.from_user.id, 'ул. Колбасная 15')
 
@@ -25,3 +25,9 @@ async def pizza_place_command(message : types.Message):
 #         await bot.send_photo(message.from_user.id, ret[0], f'{ret[1]}\nОписание: {ret[2]}\nЦена {ret[-1]}')
 
 # отправляем пользователю в личку сообщение и клавиатуру
+
+# в данной функции нужно записать команды для регистрации хэндлеров, и передать эти хэндлеры в основной файл:
+def register_handlers_client(dp : Dispatcher):
+    dp.register_message_handler(command_start, commands = ['start', 'help']) # специальный метод, который регистрирует хэндлеры для нашего бота
+    dp.register_message_handler(pizza_open_command, commands = ['Режим_работы'])
+    dp.register_message_handler(pizza_place_command, commands = ['Расположение'])
