e302dcc54020e96578f8d1fe1c289bbaeae45603
AntonUmarov <arumarov@yandex.ru>
2022-03-27 16:23:37 +0300

    end lesson 5

--- keyboards/client_kb.py
+++ keyboards/client_kb.py
@@ -12,7 +12,7 @@ kb_client = ReplyKeyboardMarkup(resize_keyboard=True) # этот класс за
 # с помощью resize_keyboard изменяем размер кнопок
 # с помощью one_time_keyboard убираем клавиатуру с экрана после нажатия кнопки
 
-kb_client.add(b1).add(b2).add(b3).row(b4, b5) # добавляем наши кнопки
+kb_client.add(b1).add(b2).add(b3) # добавляем наши кнопки
 # kb_client.add(b1).add(b2).add(b3) метод add добавляет каждую кнопку с новой строки
 # kb_client.add(b1).add(b2).insert(b3) метод insert добавляет кнопку на той же строке, если позволит место
 # kb_client.row(b1, b2, b3) метод row добавляет все кнопки в строку
