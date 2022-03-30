5a30b5407a41a1e877dd1468975e037f8d9516b3
AntonUmarov <arumarov@yandex.ru>
2022-03-30 22:21:37 +0300

    end lesson 6

--- handlers/admin.py
+++ handlers/admin.py
@@ -1,7 +1,10 @@
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram import types, Dispatcher
-from create_bot import dp
+from create_bot import dp, bot
+from aiogram.dispatcher.filters import Text
+
+ID = None
 
 class FSMAdmin(StatesGroup):
     photo = State()
@@ -9,47 +12,74 @@ class FSMAdmin(StatesGroup):
     description = State()
     price = State()
 
+# Проверяем является ли пользователь модератором группы (и модератором бота)
+# Получаем ID текущего модератора
+# Администратор должен написать не боту, а в группу
+# @dp.message_handler(commands=['moderator'], is_chat_admin=True)
+async def make_changes_command(message: types.Message):
+    global ID
+    ID = message.from_user.id
+    await bot.send_message(message.from_user.id, 'Что хозяин, нужно?') # reply_markup=button_case_admin)
+    await message.delete()
+
+
 # Начало диалога загрузки нового пункта меню
 # @dp.message_handler(commands='Загрузить', state=None)
 async def cm_start(message : types.Message):
-    await FSMAdmin.photo.set() # запускаем метод по добавлению фото
-    await message.reply('Загрузи фото') # в этом методе бот перейдет из обычного режима в режим работы машинных состояний
+    if message.from_user.id == ID:
+        await FSMAdmin.photo.set() # запускаем метод по добавлению фото
+        await message.reply('Загрузи фото') # в этом методе бот перейдет из обычного режима в режим работы машинных состояний
 
 # Ловим первый ответ и пишем в словарь
 # @dp.message_handler(content_types=['photo'], state=FSMAdmin.photo)
 async def load_photo(message: types.Message, state: FSMContext):
-    async with state.proxy() as data: # открываем словарь
-        data['photo'] = message.photo[0].file_id # записываем значение в словарь
-        # каждому файлу присваивается свой id номер, который и записывается в словарь
-    await FSMAdmin.next()
-    await message.reply("Теперь введи название")
+    if message.from_user.id == ID:
+        async with state.proxy() as data: # открываем словарь
+            data['photo'] = message.photo[0].file_id # записываем значение в словарь
+            # каждому файлу присваивается свой id номер, который и записывается в словарь
+        await FSMAdmin.next()
+        await message.reply("Теперь введи название")
 
 # Ловим второй ответ
 # @dp.message_handler(state=FSMAdmin.name)
 async def load_name(message: types.Message, state: FSMContext):
-    async with state.proxy() as data:
-        data['name'] = message.text 
-    await FSMAdmin.next()
-    await message.reply("Введи описание")
+    if message.from_user.id == ID:
+        async with state.proxy() as data:
+            data['name'] = message.text 
+        await FSMAdmin.next()
+        await message.reply("Введи описание")
 
 # Ловим третий ответ
 # @dp.message_handler(state=FSMAdmin.description)
 async def load_description(message: types.Message, state: FSMContext):
-    async with state.proxy() as data:
-        data['description'] = message.text 
-    await FSMAdmin.next()
-    await message.reply("Теперь укажи цену")
+    if message.from_user.id == ID:
+        async with state.proxy() as data:
+            data['description'] = message.text 
+        await FSMAdmin.next()
+        await message.reply("Теперь укажи цену")
 
 # Ловим последний ответ и используем полученные данные
 # @dp.message_handler(state=FSMAdmin.price)
 async def load_price(message: types.Message, state: FSMContext):
-    async with state.proxy() as data:
-        data['price'] = float(message.text) 
+    if message.from_user.id == ID:
+        async with state.proxy() as data:
+            data['price'] = float(message.text) 
    
-    async with state.proxy() as data:
-        await message.reply(str(data)) # выводим то, что записано в словарь
-   # после выполнения следующей команды бот выходит из машинных состояний и очищает то, что было записано
-    await state.finish() 
+        async with state.proxy() as data:
+            await message.reply(str(data)) # выводим то, что записано в словарь
+       # после выполнения следующей команды бот выходит из машинных состояний и очищает то, что было записано
+        await state.finish() 
+
+# Выход из состояний
+# @dp.message_handler(state="*", commands='отмена')
+# @dp.message_handler(Text(equals='отмена', ignore_case=True), state="*")
+async def cancel_handler(message: types.Message, state: FSMContext):
+    if message.from_user.id == ID:
+        current_state = await state.get_state()
+        if current_state is None:
+            return
+        await state.finish()
+        await message.reply('OK')
 
 # Регистрируем хэндлеры
 def register_handlers_admin(dp : Dispatcher):
@@ -58,3 +88,6 @@ def register_handlers_admin(dp : Dispatcher):
     dp.register_message_handler(load_name, state=FSMAdmin.name)
     dp.register_message_handler(load_description, state=FSMAdmin.description)
     dp.register_message_handler(load_price, state=FSMAdmin.price)
+    dp.register_message_handler(cancel_handler, state="*", commands='отмена')
+    dp.register_message_handler(cancel_handler, Text(equals='отмена', ignore_case=True), state="*")
+    dp.register_message_handler(make_changes_command, commands=['moderator'], is_chat_admin=True)
